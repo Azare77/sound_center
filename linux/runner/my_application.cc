8bd6cda0fdaa452456851ec7495580687fc1a187
Ali zare <a.zare.developer@gmail.com>
2026-08-27 22:05:24 +0330

    change default window size for linux

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -54,7 +54,7 @@ static void my_application_activate(GApplication *application) {
         gtk_window_set_title(window, "Sound Center");
     }
 
-    gtk_window_set_default_size(window, 400, 900);
+    gtk_window_set_default_size(window, 1000, 500);
     gtk_widget_show(GTK_WIDGET(window));
 
     g_autoptr(FlDartProject)
